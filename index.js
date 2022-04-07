0dfba07ebf8e475e53da9693c0f4e4de49fdf0b4
Denis <Danielarbete@protonmail.com>
2022-04-07 12:27:12 +0200

    Change and implement npm start command to active Rest API and its functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("#penis");
+import express from "express"
+import bodyParser from "body-parser";
+
+
+const app = express();
+const PORT = 8080;
+app.use(bodyParser.json())
+app.listen(PORT,()=>
+    console.log(PORT,`fuck you at http://localhost:${PORT}`)
+)
+
+app.get("/",(reg,res)=>{
+    console.log("fuck you was taken")
+    res.send("Fuck you")
+})
